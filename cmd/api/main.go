9bb7a074cf7a9fff0446ad98673fae12b595f952
ndinhbang <ndinhbang111@gmail.com>
2026-04-17 13:21:56 +0700

    Integrate Echo framework for API development
    - Added Echo v5.1.0 as a dependency in go.mod.
    - Introduced basic API server setup in main.go with request logging and recovery middleware.
    - Implemented a simple "Hello, World!" route.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.RequestLogger()) // use the RequestLogger middleware with slog logger
+	e.Use(middleware.Recover())       // recover panics as errors for proper error handling
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	if err := e.Start(":8080"); err != nil {
+		slog.Error("failed to start server", "error", err)
+	}
+}
+
+// Handler
+func hello(c *echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module ndinhbang/go-skeleton
 
 go 1.26.1
+
+require github.com/labstack/echo/v5 v5.1.0
+
+require golang.org/x/time v0.14.0 // indirect
